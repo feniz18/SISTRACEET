ead5c0dafe6ad9217fe3497945d54bfe085749f2
feniz18 <fenizhc18@gmail.com>
2018-05-06 17:02:56 -0500

    Se agrega contenido de perfil y slider a integrantes

--- resources/views/compartida/integrantes/integrantes.blade.php
+++ resources/views/compartida/integrantes/integrantes.blade.php
@@ -40,14 +40,42 @@
 
 
         <div class="row" id="contenido" style="display:none">
-          <div class="col-md-8">
+          <div class="col-md-5">
             <div class="box">
               <div class="box-header">
-                <h3 class="box-title">Instructores registrados en sistema</h3>
+                <h3 class="box-title">Integrantes Sistraceet</h3>
               </div>
               <!-- /.box-header -->
               <div class="box-body">
-               
+
+                <div class="box box-widget widget-user">
+                  <!-- Add the bg color to the header using any of the bg-* classes -->
+                  <div class="widget-user-header bg-red-active">
+                    <h3 class="widget-user-username">lUIS AGUSTIN BLANCO</h3>
+                    <h5 class="widget-user-desc">TECNOLOGO ADSI</h5>
+                  </div>
+                  <div class="widget-user-image">
+                    <img class="img-circle" src="{{ asset('img/integrantes/luis.jpeg') }}" alt="User Avatar">
+                  </div>
+                  <div class="box-footer">
+                    <div class="row">
+                      <div class="col-sm-12 border-right">
+                        <div class="description-block" style="text-align: justify;">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <!-- /.description-block -->
+                      </div>
+                      <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                  </div>
+                </div>
+
               </div>
 
             <!-- /.box-body -->
@@ -55,14 +83,50 @@
           <!-- /.box -->
           </div>
           <!-- /.col -->
-          <div class="col-md-4">
+          <div class="col-md-7">
                   <div class="box box-danger">
                     <div class="box-header with-border">
-                      <h3 class="box-title">Instructores registrados en sistema</h3>
+                      <h3 class="box-title">Información detallada</h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                     
+                      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                          <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+                          <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+                        </ol>
+                        <div class="carousel-inner">
+                          <div class="item active">
+                            <img src="http://placehold.it/900x500/39CCCC/ffffff&amp;text=I+Love+Bootstrap" alt="First slide">
+
+                            <div class="carousel-caption">
+                              First Slide
+                            </div>
+                          </div>
+                          <div class="item">
+                            <img src="http://placehold.it/900x500/3c8dbc/ffffff&amp;text=I+Love+Bootstrap" alt="Second slide">
+
+                            <div class="carousel-caption">
+                              Second Slide
+                            </div>
+                          </div>
+                          <div class="item">
+                            <img src="http://placehold.it/900x500/f39c12/ffffff&amp;text=I+Love+Bootstrap" alt="Third slide">
+
+                            <div class="carousel-caption">
+                              Third Slide
+                            </div>
+                          </div>
+                        </div>
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                          <span class="fa fa-angle-left"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                          <span class="fa fa-angle-right"></span>
+                        </a>
+                      </div>
+                             
                     </div>
 
                   <!-- /.box-body -->
